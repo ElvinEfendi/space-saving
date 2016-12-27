f0ed5dc912a3192b09d89a9e2468b9a69223980a
Elvin Efendi <elvin.efendiyev@gmail.com>
2016-12-27 17:08:05 -0500

    refactoring

--- lib/space_saving.lua
+++ lib/space_saving.lua
@@ -4,7 +4,9 @@ local _M = {}
 _M.version = "0.0.1"
 _M.__index = _M
 
-function _M.new(dict_name, window, phi, epsilon, number_of_counters)
+-- initialize an instance of algorithm with given parameters using
+-- the giving shared dict and window(in seconds) size
+function _M.new(dict_name, window, phi, epsilon, max_counters_size)
   local dict = ngx.shared[dict_name]
   if not dict then
     return nil, "shared dict not found"
@@ -16,49 +18,76 @@ function _M.new(dict_name, window, phi, epsilon, number_of_counters)
   -- this is the error rate we allow, i.e f^(i) >= (phi-epsilon)*number_of_hits
   local epsilon = epsilon or 0.001
   -- according to the paper 1/epsilon is the safest value to guarantee f_i > epsilon*number_of_hits
-  local number_of_counters = number_of_counters or 1/epsilon
+  local max_counters_size = max_counters_size or 1/epsilon
 
-  return setmetatable({ dict = dict, phi = phi, window = window, epsilon = epsilon, number_of_counters = number_of_counters }, _M)
+  return setmetatable({ dict = dict, phi = phi, window = window, epsilon = epsilon, max_counters_size = max_counters_size }, _M)
 end
 
-function _M:process(key, weight)
-  local weight = weight or 1
+local function calculate_min_and_size(counters)
+  local min_count = math.huge
+  local min_count_key
+  local counters_size = 0
+  for key, stats in pairs(counters) do
+    counters_size = counters_size + 1
+    if stats.counters < min_count then
+      min_count = stats.count
+      min_count_index = key
+    end
+  end
+  return min_count, min_count_key, counters_size
+end
+
+local function get_or_init_counters(self)
   local counters_json
   local hits, err = self.dict:incr("hits", 1)
-  if not hits and err == "not found" then
+  if hits then
+    counters_json = self.dict:get("counters")
+  elseif err ~= "not found" then
+    return nil, err
+  else
     counters_json = "{}"
     self.dict:set("counters", counters_json, self.window)
     self.dict:add("hits", 1, self.window)
-  else
-    counters_json = self.dict:get("counters")
   end
 
   local ok, counters = pcall(json.decode, counters_json)
+  if not ok then
+    return nil, string.format("Could not decode counters, error: %s", counters)
+  end
+  return counters, nil
+end
+
+local function update_counters(self, counters)
+  local ok, counters_json = pcall(json.encode, counters)
+  if not ok then
+    return string.format("Could not encode counters, error: %s", counters_json)
+  end
+  local ok, err = self.dict:replace("counters", counters_json)
+  if not ok then
+    return err
+  end
+  return nil
+end
+
+function _M:process(key)
+  local counters, err = get_or_init_counters(self)
+  if err then
+    return err
+  end
   local stats = counters[key]
   if stats then
-    stats.weight = stats.weight + 1
+    stats.count = stats.count + 1
   else
-    local min_weight = math.huge
-    local min_weight_index
-    local current_number_of_counters = 0
-    for key, stats in pairs(counters) do
-      current_number_of_counters = current_number_of_counters + 1
-      if stats.weight < min_weight then
-        min_weight = stats.weight
-        min_weight_index = key
-      end
-    end
-    if current_number_of_counters < self.number_of_counters then
-      stats = { weight = 1, overestimation = 0 }
+    local min_count, min_count_key, counters_size = calculate_min_and_size(counters)
+    if counters_size < self.max_counters_size then
+      stats = { count = 1, overestimation = 0 }
     else
-      counters[min_weight_index] = nil
-      stats = { weight = min_weight + weight, overestimation = min_weight }
+      counters[min_count_index] = nil
+      stats = { count = min_count + 1, overestimation = min_count }
     end
     counters[key] = stats
   end
-
-  local ok, counters_json = pcall(json.encode, counters)
-  self.dict:replace("counters", counters_json)
+  return update_counters(self, counters)
 end
 
 function _M:report(key)
